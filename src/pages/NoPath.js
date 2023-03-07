f529766ea1b65532539fd8e89a98340957739aaa
jiyoung-B <catgirl0313@naver.com>
2023-03-07 12:51:48 +0900

    NavLink로 변경, NoPath 추가

---
+++ src/pages/NoPath.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useLocation} from "react-router-dom";
+const NoPath = () => {
+    return (
+        <div>
+            <h2>No Path</h2>
+            <p>404 - No Resource!!</p>
+        </div>
+    );
+};
+
+
+export default NoPath;
